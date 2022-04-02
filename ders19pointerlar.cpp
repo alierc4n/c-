ab3c041fb4459f39705c98ec978cf633002ef271
alierc4n <102850552+alierc4n@users.noreply.github.com>
2022-04-02 17:51:37 +0300

    Add files via upload

---
+++ ders19pointerlar.cpp
@@ -0,0 +1,19 @@
+﻿#include <iostream>
+using namespace std;
+
+int main() {
+
+	int a = 5;
+
+	int *ptr = &a;
+
+	cout << "degiskenin adresi: " << &a << endl;
+	cout << "degiskenin degeri: " << *ptr << endl;
+	
+	*ptr = 6;
+
+	cout << "degiskenin yeni degeri: " << a << endl;
+
+
+	return 0;
+}

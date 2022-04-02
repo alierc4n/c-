e2703af3634d7ebba802dcde3deed25fe31753fd
alierc4n <102850552+alierc4n@users.noreply.github.com>
2022-04-02 15:45:14 +0300

    Add files via upload

---
+++ ders16switchcase.cpp
@@ -0,0 +1,29 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main() {
+
+	int islem;
+
+	cout << "islemi giriniz: ";
+	cin >> islem;
+	
+	switch (islem)
+	{
+
+	case 1: cout << "1. islemi sectiniz: " << endl;
+		break;
+	case 2: cout << "2. islemi sectiniz: " << endl;
+		break;
+	case 3: cout << "3. islemi sectiniz: " << endl;
+		break;
+	case 4: cout << "4. islemi sectiniz: " << endl;
+		break;
+	default:
+		cout << "gecersiz islem!.. " << endl;
+		break;
+	}
+
+	return 0;
+}

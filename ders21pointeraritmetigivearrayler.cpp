725a4f98522f77238fd08175595b12bbd4f92626
alierc4n <102850552+alierc4n@users.noreply.github.com>
2022-04-03 13:59:02 +0300

    Add files via upload

---
+++ ders21pointeraritmetigivearrayler.cpp
@@ -0,0 +1,16 @@
+﻿#include<iostream>
+using namespace std;
+
+int main() {
+
+	string array[] = { "mustafa","murat","ali" };
+
+	string* ptr = array;
+
+	cout << ptr + 1 << endl;
+	cout << *(ptr + 1) << endl;
+
+	cout << ptr[1] << endl;
+
+	return 0;
+}

07e0aa4160533e022d051f9e8511d04dca0a1464
alierc4n <102850552+alierc4n@users.noreply.github.com>
2022-04-03 13:26:40 +0300

    Add files via upload

---
+++ ders20pointerlarinkullanmamantigi.cpp
@@ -0,0 +1,17 @@
+﻿#include <iostream>
+using namespace std;
+
+void degerDegistir(int af) {
+
+	af = 20;
+	cout << "fonksiyon icindeki deger: " << af << endl;
+}
+
+int main() {
+
+	int a = 10;
+	cout << "a degiskeninin degeri: " << a << endl;
+	degerDegistir(a);
+	cout << "fonksiyon bittikten sonra a degiskeninin degeri: "<< a << endl;
+	return 0;
+}

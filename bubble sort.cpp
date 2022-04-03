8976e0db3d21c44a3b4873546823cfb9ee054522
alierc4n <102850552+alierc4n@users.noreply.github.com>
2022-04-03 18:20:16 +0300

    Add files via upload

---
+++ bubble sort.cpp
@@ -0,0 +1,40 @@
+﻿#include <iostream>
+
+using namespace std;
+
+/*
+
+Bubble Sort
+Kabarcık Sıralaması
+
+1 17 10 9 12 20
+1 10 9 12 17 20
+1 9 10 12 17 20
+...
+...
+...
+
+*/
+
+int main() {
+
+	int dizi[6] = { 1,20,17,10,9,12 };
+
+	for (int adim = 1; adim < 6; adim++) {
+		for (int j = 0; j < 6 - adim; j++) {
+			if (dizi[j] > dizi[j + 1]) {
+				int araciEleman = dizi[j];
+				dizi[j] = dizi[j + 1];
+				dizi[j + 1] = araciEleman;
+			}
+		}
+	}
+
+	// Ekrana yazdirma
+
+	for (int a = 0; a < 6; a++) {
+		cout << dizi[a] << "\t";
+	}
+
+	return 0;
+}

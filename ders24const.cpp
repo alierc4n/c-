9f1bfa9718ae26a9e946fd29746ef58df423afb5
alierc4n <102850552+alierc4n@users.noreply.github.com>
2022-04-03 15:25:29 +0300

    Add files via upload

---
+++ ders24const.cpp
@@ -0,0 +1,17 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main() {
+
+	const int a[] = { 10,20,30,40,50 };
+
+//	a[2] = 100; yanlış kullanım
+
+
+//	const int i = 32;
+
+//	cout << i << endl;
+
+	return 0;
+}
